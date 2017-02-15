f83e297c4028e91640ab40f716ed36c538186a91
gjh <642558392@qq.com>
2017-02-15 14:26:17 +0800

    总结不能作为插件使用的原因

--- js/index.js
+++ js/index.js
@@ -58,3 +58,13 @@ lis.add('#box section').hover(function(){
 },function(){
 	autoPlay();
 })
+
+/*
+ 
+ * 中午遗留问题:想一想我们的轮播图能封装成插件吗？会产生什么问题？
+ * 1.插件中最好不要使用id，原因：插件是能够被重复使用，也就是说在同一页面中可能多次使用，造成冲突。
+ * 2.变量的命名和方法的命名: states  ,interval , move() ,next(),用户在使用这个插件的时候，可以还会引起
+ *   自己创建的 js 文件，也有这样的命名，那么就产生冲突了。
+ * 3.标签 class 的值得问题：prev , next ，这些 class太大众化了，谁写标签都想取这个prve 和 next. 势必会冲突。
+ * 4.插件文件名问题： index.js ,index.css 命名太大众化 ，比如这样修改:jQuery.ZYSlide.js。
+ * */
